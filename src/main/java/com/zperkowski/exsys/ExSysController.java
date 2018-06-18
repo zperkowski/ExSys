f7439b028e80d4b14c2aaa64bb73eb67733f2c8b
Zdzisław Perkowski <ZdzislawPerkowski@outlook.com>
2018-06-18 23:43:21 +0200

    Created GUI for the main program

--- src/main/java/com/zperkowski/exsys/ExSysController.java
+++ src/main/java/com/zperkowski/exsys/ExSysController.java
@@ -1,4 +1,35 @@
 package com.zperkowski.exsys;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
 public class ExSysController {
+
+    private KieSession session;
+
+    @FXML
+    Label ex_label_question;
+    @FXML
+    ComboBox ex_combo_answer;
+
+    @FXML
+    void initialize() {
+        KieServices ks = KieServices.Factory.get();
+        BasicConfigurator.configure();
+        Logger.getLogger(ExSysController.class).setLevel(Level.OFF);
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        session = kContainer.newKieSession("exsys-rules");
+    }
+
+    @FXML
+    public void ex_button_next_clicked() {
+
+    }
 }
